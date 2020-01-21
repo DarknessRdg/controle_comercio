e4d72262af2e80289d75b8f524370b6d7b0b1821
Luan Rodrigues <luanrodrigues007@hotmail.com>
2020-01-21 02:41:17 -0300

    first buidl

--- README.md
+++
@@ -1,2 +0,0 @@
-# controle_comercio
-Projeto para controlar vendas e funcionários de uma lojinha física
---
+++ precache-manifest.a418e3ea5fd941131b0d3cf7685bbbe7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "797dfe2dab4832370c2802e003da980e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "496dbc71ac4b2b8286d6",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "06bc4e8e23f85e23c4ed",
+    "url": "/static/js/2.7c67f299.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.7c67f299.chunk.js.LICENSE"
+  },
+  {
+    "revision": "496dbc71ac4b2b8286d6",
+    "url": "/static/js/main.2fc37049.chunk.js"
+  },
+  {
+    "revision": "b79d75fd4bc54d53c002",
+    "url": "/static/js/runtime-main.23c8ef9d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
